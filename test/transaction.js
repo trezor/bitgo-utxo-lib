fd2418295ad3a6d87d0639c1d92f2e7bf0dc7ef2
Szymon Lesisz <sz.lesisz@gmail.com>
2019-03-29 16:59:33 +0100

    add test for Dogecoin (tx values as string)

--- test/transaction.js
+++ test/transaction.js
@@ -181,6 +181,52 @@ describe('Transaction', function () {
     })
   })
 
+  describe('fromBuffer/fromHex for DOGE', function () {
+    fixtures.doge.valid.forEach(function (testData) {
+      it('imports ' + testData.description, function () {
+        Transaction.USE_STRING_VALUES = true
+        const tx = Transaction.fromHex(testData.hex, networks.doge)
+        assert.equal(tx.ins.length, testData.ins.length)
+        assert.equal(tx.outs.length, testData.outs.length)
+        for (var i = 0; i < tx.ins.length; i++) {
+          assert.equal(tx.ins[i].hash.toString('hex'), testData.ins[i].hash)
+          assert.equal(tx.ins[i].index, testData.ins[i].index)
+          assert.equal(tx.ins[i].script.toString('hex'), testData.ins[i].script)
+          assert.equal(tx.ins[i].sequence, testData.ins[i].sequence)
+        }
+
+        for (var i = 0; i < tx.outs.length; i++) {
+          assert.equal(tx.outs[i].value, testData.outs[i].value)
+          assert.equal(tx.outs[i].script.toString('hex'), testData.outs[i].script)
+        }
+        Transaction.USE_STRING_VALUES = false
+      })
+    })
+
+    fixtures.doge.valid.forEach(function (testData) {
+      it('exports ' + testData.description, function () {
+        Transaction.USE_STRING_VALUES = true
+        const tx = Transaction.fromHex(testData.hex, networks.doge)
+        const hashTx = tx.getId()
+        const hexTx = tx.toHex()
+        assert.equal(testData.hex, hexTx)
+        assert.equal(testData.hash, hashTx)
+        Transaction.USE_STRING_VALUES = false
+      })
+    })
+
+    fixtures.doge.valid.forEach(function (testData) {
+      it('clone ' + testData.description, function () {
+        Transaction.USE_STRING_VALUES = true
+        const tx = Transaction.fromHex(testData.hex, networks.doge)
+        const clonedTx = tx.clone()
+        assert.equal(clonedTx.toHex(), testData.hex)
+        Transaction.USE_STRING_VALUES = false
+      })
+    })
+
+  })
+
   describe('toBuffer/toHex', function () {
     fixtures.valid.forEach(function (f) {
       it('exports ' + f.description + ' (' + f.id + ')', function () {
