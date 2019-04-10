0cf019eb80f25389ec415aa7a5b8e5a8a08a08b1
Szymon Lesisz <sz.lesisz@gmail.com>
2019-04-10 13:55:59 +0200

    update trezor tests

--- test/trezor.js
+++ test/trezor.js
@@ -1,5 +1,3 @@
-// Additional tests for Trezor custom changes
-
 /* global describe, it */
 
 var assert = require('assert')
@@ -7,29 +5,56 @@ var networks = require('../src/networks')
 
 var fixturesTransaction = require('./fixtures/transaction')
 var fixturesBufferutils = require('./fixtures/bufferutils.json')
+var fixturesTrezor = require('./fixtures/trezor.json')
 var Transaction = require('../src/transaction')
 var bufferutils = require('../src/bufferutils')
 
+// Additional tests for Trezor custom changes
+
 describe('Trezor', function () {
-  describe('Transaction: fromBuffer/fromHex for DOGE (values as string)', function () {
-    const fixtures = fixturesTransaction.doge.valid
+
+  describe('Transaction: toHex with invalid output values', function () {
+    const fixtures = fixturesTrezor.transaction.invalid
+    Transaction.USE_STRING_VALUES = true
+    fixtures.forEach(function (f) {
+      it('throws on ' + f.description, function () {
+        assert.throws(function () {
+          var tx = Transaction.fromHex(f.hex)
+          tx.outs.forEach(function (o) {
+            o.value = f.value
+          })
+          tx.toHex()
+        }, new RegExp(f.exception))
+      })
+    })
+    Transaction.USE_STRING_VALUES = false
+  })
+
+  describe('Trezor Transaction: fromBuffer/fromHex with values as strings', function () {
+    const fixtures = fixturesTrezor.transaction.valid
+    // const fixtures = fixturesTransaction.valid
+    
     fixtures.forEach(function (testData) {
       it('imports ' + testData.description, function () {
         Transaction.USE_STRING_VALUES = true
-        const tx = Transaction.fromHex(testData.hex, networks.doge)
-        assert.equal(tx.ins.length, testData.ins.length)
-        assert.equal(tx.outs.length, testData.outs.length)
+        const tx = Transaction.fromHex(testData.hex, testData.network)
+        assert.strictEqual(tx.ins.length, testData.ins.length)
         tx.ins.forEach(function (input, i) {
-          assert.equal(input.hash.toString('hex'), testData.ins[i].hash)
-          assert.equal(input.index, testData.ins[i].index)
-          assert.equal(input.script.toString('hex'), testData.ins[i].script)
-          assert.equal(input.sequence, testData.ins[i].sequence)
+          assert.strictEqual(input.hash.toString('hex'), testData.ins[i].hash)
+          assert.strictEqual(input.index, testData.ins[i].index)
+          assert.strictEqual(input.script.toString('hex'), testData.ins[i].script)
+          assert.strictEqual(input.sequence, testData.ins[i].sequence)
         })
-
+        
+        assert.strictEqual(tx.outs.length, testData.outs.length)
         tx.outs.forEach(function (output, i) {
-          assert.equal(output.value, testData.outs[i].value)
+          assert.strictEqual(output.value, testData.outs[i].value.toString())
           assert.equal(output.script.toString('hex'), testData.outs[i].script)
         })
+        
+        if (testData.time) {
+          assert.strictEqual(tx.timestamp, testData.time)
+        }
         Transaction.USE_STRING_VALUES = false
       })
     })
@@ -37,11 +62,8 @@ describe('Trezor', function () {
     fixtures.forEach(function (testData) {
       it('exports ' + testData.description, function () {
         Transaction.USE_STRING_VALUES = true
-        const tx = Transaction.fromHex(testData.hex, networks.doge)
-        const hashTx = tx.getId()
-        const hexTx = tx.toHex()
-        assert.equal(testData.hex, hexTx)
-        assert.equal(testData.hash, hashTx)
+        const tx = Transaction.fromHex(testData.hex, testData.network)
+        assert.strictEqual(testData.hex, tx.toHex())
         Transaction.USE_STRING_VALUES = false
       })
     })
@@ -49,7 +71,66 @@ describe('Trezor', function () {
     fixtures.forEach(function (testData) {
       it('clone ' + testData.description, function () {
         Transaction.USE_STRING_VALUES = true
-        const tx = Transaction.fromHex(testData.hex, networks.doge)
+        const tx = Transaction.fromHex(testData.hex, testData.network)
+        const clonedTx = tx.clone()
+        assert.strictEqual(clonedTx.toHex(), testData.hex)
+        Transaction.USE_STRING_VALUES = false
+      })
+    })
+  })
+
+
+  // copied from ./test/transaction.js
+  describe('Zcash Transaction: fromBuffer/fromHex with values as strings', function () {
+    const fixtures = fixturesTransaction.zcash.valid
+    fixtures.forEach(function (testData) {
+      it('imports ' + testData.description, function () {
+        Transaction.USE_STRING_VALUES = true
+        const tx = Transaction.fromHex(testData.hex, networks.zcashTest)
+        assert.equal(tx.version, testData.version)
+        assert.equal(tx.versionGroupId, parseInt(testData.versionGroupId, 16))
+        assert.equal(tx.overwintered, testData.overwintered)
+        assert.equal(tx.locktime, testData.locktime)
+        assert.equal(tx.expiryHeight, testData.expiryHeight)
+        assert.equal(tx.ins.length, testData.insLength)
+        assert.equal(tx.outs.length, testData.outsLength)
+        assert.equal(tx.joinsplits.length, testData.joinsplitsLength)
+        assert.equal(tx.joinsplitPubkey.length, testData.joinsplitPubkeyLength)
+        assert.equal(tx.joinsplitSig.length, testData.joinsplitSigLength)
+
+        if (testData.valueBalance) {
+          assert.equal(tx.valueBalance, testData.valueBalance)
+        }
+        if (testData.nShieldedSpend > 0) {
+          for (var i = 0; i < testData.nShieldedSpend; ++i) {
+            assert.equal(tx.vShieldedSpend[i].cv.toString('hex'), testData.vShieldedSpend[i].cv)
+            assert.equal(tx.vShieldedSpend[i].anchor.toString('hex'), testData.vShieldedSpend[i].anchor)
+            assert.equal(tx.vShieldedSpend[i].nullifier.toString('hex'), testData.vShieldedSpend[i].nullifier)
+            assert.equal(tx.vShieldedSpend[i].rk.toString('hex'), testData.vShieldedSpend[i].rk)
+            assert.equal(tx.vShieldedSpend[i].zkproof.sA.toString('hex') +
+              tx.vShieldedSpend[i].zkproof.sB.toString('hex') +
+              tx.vShieldedSpend[i].zkproof.sC.toString('hex'), testData.vShieldedSpend[i].zkproof)
+            assert.equal(tx.vShieldedSpend[i].spendAuthSig.toString('hex'), testData.vShieldedSpend[i].spendAuthSig)
+          }
+        }
+        Transaction.USE_STRING_VALUES = false
+      })
+    })
+
+    fixtures.forEach(function (testData) {
+      it('exports ' + testData.description, function () {
+        Transaction.USE_STRING_VALUES = true
+        const tx = Transaction.fromHex(testData.hex, networks.zcashTest)
+        const hexTx = tx.toHex()
+        assert.equal(testData.hex, hexTx)
+        Transaction.USE_STRING_VALUES = false
+      })
+    })
+
+    fixtures.forEach(function (testData) {
+      it('clone ' + testData.description, function () {
+        Transaction.USE_STRING_VALUES = true
+        const tx = Transaction.fromHex(testData.hex, networks.zcashTest)
         const clonedTx = tx.clone()
         assert.equal(clonedTx.toHex(), testData.hex)
         Transaction.USE_STRING_VALUES = false
@@ -57,58 +138,85 @@ describe('Trezor', function () {
     })
   })
 
-  describe('Transaction: fromBuffer/fromHex for Capricoin (timestamp)', function () {
-    const fixtures = fixturesTransaction.capricoin
+  describe('Dash Testnet Transaction: fromBuffer/fromHex values as strings', function () {
+    const fixtures = fixturesTransaction.dasht.valid
     fixtures.forEach(function (testData) {
       it('imports ' + testData.description, function () {
-        const tx = Transaction.fromHex(testData.hex, networks.capricoin)
-        assert.equal(tx.timestamp, testData.time)
+        Transaction.USE_STRING_VALUES = true
+        const tx = Transaction.fromHex(testData.hex, networks.dashTest)
+        assert.equal(tx.version, testData.version)
+        if (tx.version === 3) {
+          assert.equal(tx.type, testData.type)
+        }
+        assert.equal(tx.locktime, testData.locktime)
+        assert.equal(tx.ins.length, testData.vin.length)
+        assert.equal(tx.outs.length, testData.vout.length)
+        tx.outs.forEach(function (output, i) {
+          const fixtureValue = Math.round(testData.vout[i].value * 100000000);
+          assert.strictEqual(output.value, fixtureValue.toString())
+          assert.equal(output.script.toString('hex'), testData.vout[i].scriptPubKey.hex)
+        })
+        if (tx.isDashSpecialTransaction()) {
+          assert.equal(tx.extra_payload.toString('hex'), testData.extraPayload)
+        }
+        Transaction.USE_STRING_VALUES = false
       })
     })
 
     fixtures.forEach(function (testData) {
       it('exports ' + testData.description, function () {
-        const tx = Transaction.fromHex(testData.hex, networks.capricoin)
-        const hashTx = tx.getId()
+        Transaction.USE_STRING_VALUES = true
+        const tx = Transaction.fromHex(testData.hex, networks.dashTest)
         const hexTx = tx.toHex()
         assert.equal(testData.hex, hexTx)
-        assert.equal(testData.hash, hashTx)
+        Transaction.USE_STRING_VALUES = false
       })
     })
 
     fixtures.forEach(function (testData) {
       it('clone ' + testData.description, function () {
-        const tx = Transaction.fromHex(testData.hex, networks.capricoin)
+        Transaction.USE_STRING_VALUES = true
+        const tx = Transaction.fromHex(testData.hex, networks.dashTest)
         const clonedTx = tx.clone()
-        assert.equal(clonedTx.timestamp, testData.time)
+        assert.equal(clonedTx.toHex(), testData.hex)
+        Transaction.USE_STRING_VALUES = false
       })
     })
   })
 
   describe('Transaction: getExtraData - Dash', function () {
-    const fixtures = fixturesTransaction.dasht.valid.filter(t => typeof t.extraPayload === 'string')
-    fixtures.forEach(function (testData) {
+    const fixtures = fixturesTransaction.dasht.valid;
+    const trezorFixtures = fixturesTrezor.transaction.extraData.dasht
+    // const fixtures = fixturesTransaction.dasht.valid.filter(t => typeof t.extraPayload === 'string')
+    fixtures.forEach(function (testData, i) {
       it('imports ' + testData.description, function () {
+        Transaction.USE_STRING_VALUES = true
         const tx = Transaction.fromHex(testData.hex, networks.dashTest)
-        const extra = tx.getExtraData().toString('hex')
-        assert.equal(extra, '26' + testData.extraPayload) // additional data required by Trezor
+        const extra = tx.getExtraData()
+        const extraData = extra ? extra.toString('hex') : null
+        assert.equal(extraData, trezorFixtures[i])
+        Transaction.USE_STRING_VALUES = false
       })
     })
   })
 
   describe('Transaction: getExtraData - Zcash', function () {
     const fixtures = fixturesTransaction.zcash.valid
-    fixtures.forEach(function (testData) {
+    const trezorFixtures = fixturesTrezor.transaction.extraData.zcash
+    fixtures.forEach(function (testData, i) {
       it('imports ' + testData.description, function () {
+        Transaction.USE_STRING_VALUES = true
         const tx = Transaction.fromHex(testData.hex, networks.zcashTest)
-
-        tx.getExtraData()
+        const extra = tx.getExtraData()
+        const extraData = extra ? extra.toString('hex') : null
+        assert.equal(extraData, trezorFixtures[i])
+        Transaction.USE_STRING_VALUES = false
       })
     })
   })
 
   describe('bufferutils: writeUInt64LEasString', function () {
-    const fixtures = fixturesBufferutils.writeUInt64LEasString.valid
+    const fixtures = fixturesTrezor.bufferutils.writeUInt64LEasString
     fixtures.forEach(function (f) {
       it('encodes ' + f.description + ' correctly', function () {
         var buffer = Buffer.alloc(8, 0)
@@ -119,8 +227,7 @@ describe('Trezor', function () {
   })
 
   describe('bufferutils: readUInt64LEasString', function () {
-    const fixtures = fixturesBufferutils
-    fixtures.valid.forEach(function (f) {
+    fixturesBufferutils.valid.forEach(function (f) {
       it('decodes ' + f.hex64 + ' correctly', function () {
         var buffer = Buffer.from(f.hex64, 'hex')
         var string = bufferutils.readUInt64LEasString(buffer, 0)
@@ -128,7 +235,7 @@ describe('Trezor', function () {
       })
     })
 
-    fixtures.readUInt64LEasString.valid.forEach(function (f) {
+    fixturesTrezor.bufferutils.readUInt64LEasString.forEach(function (f) {
       it('decodes ' + f.description + ' correctly', function () {
         var buffer = Buffer.from(f.hex64, 'hex')
         var string = bufferutils.readUInt64LEasString(buffer, 0)
