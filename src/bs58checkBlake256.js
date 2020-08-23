e843dfbaebc197b5c73f72d49c2cdf35370fc62e
JoeGruff <joegruffins@gmail.com>
2020-08-23 13:30:50 +0900

    hdnode: Use blake256 for decred.

--- src/bs58checkBlake256.js
+++ src/bs58checkBlake256.js
@@ -5,6 +5,21 @@ var bs58 = require('bs58')
 function decodeBlake256 (address) {
   var buffer = bs58.decode(address)
   if (buffer.length !== 26) throw new Error(address + ' invalid address length')
+  var payload
+  try {
+    payload = decode(buffer)
+  } catch (e) {
+    throw new Error(address + ' ' + e.message)
+  }
+  return payload
+}
+
+function decodeBlake256Key (key) {
+  var buffer = bs58.decode(key)
+  return decode(buffer)
+}
+
+function decode (buffer) {
   var want = buffer.slice(-4)
   var payload = buffer.slice(0, -4)
   var got = bblake256(bblake256(payload)).slice(0, 4)
@@ -12,7 +27,7 @@ function decodeBlake256 (address) {
   if (want[0] ^ got[0] |
       want[1] ^ got[1] |
       want[2] ^ got[2] |
-      want[3] ^ got[3]) throw new Error(address + ' invalid checksum')
+      want[3] ^ got[3]) throw new Error('invalid checksum')
 
   return payload
 }
@@ -24,5 +39,6 @@ function encodeBlake256 (payload) {
 
 module.exports = {
   encodeBlake256: encodeBlake256,
-  decodeBlake256: decodeBlake256
+  decodeBlake256: decodeBlake256,
+  decodeBlake256Key: decodeBlake256Key
 }
